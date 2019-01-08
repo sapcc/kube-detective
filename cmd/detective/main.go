8b25c47b3f1d5b574adccd2bcfba93c7d9b40973
Michael Schmidt <michael02.schmidt@sap.com>
2019-01-08 12:02:03 +0100

    small refactoring. harhar

--- cmd/detective/main.go
+++ cmd/detective/main.go
@@ -2,38 +2,41 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"os"
 	"os/signal"
 	"syscall"
 
 	"github.com/sapcc/kube-detective/pkg/detective"
+	"k8s.io/klog"
 )
 
 var opts detective.Options
 
 func init() {
 	flag.StringVar(&opts.ExternalCIDR, "externalCIDR", "", "subnet used for external IPs")
+	flag.StringVar(&opts.NodeFilterRegex, "nodeFilter", ".*", "filter node names with this regex")
 }
 
 func main() {
+	flag.Set("alsologtostderr", "true")
 	flag.Parse()
 
+	klogFlags := flag.NewFlagSet("klog", flag.ExitOnError)
+	klog.InitFlags(klogFlags)
+
 	sigs := make(chan os.Signal, 1)
-	done := make(chan bool, 1)
 	signal.Notify(sigs, os.Interrupt, syscall.SIGTERM)
 
 	d := detective.NewDetective(opts)
 
 	go func() {
-		d.Run()
-		done <- true
-	}()
-
-	go func() {
 		<-sigs
-		d.Stop()
-		done <- true
+		d.Kill()
 	}()
 
-	<-done
+	if err := d.Wait(); err != nil {
+		fmt.Printf("Error: %v\n", err)
+		os.Exit(1)
+	}
 }
